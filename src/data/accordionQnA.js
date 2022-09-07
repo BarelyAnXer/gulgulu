81e1ba6fc385e2d459a8eadf741edb4dd9b3628b
DreamIce-1 <christiangazzingan38@gmail.com>
2022-09-07 20:03:29 +0800

    added details to qna

--- src/data/accordionQnA.js
+++ src/data/accordionQnA.js
@@ -1,14 +1,18 @@
 export const accordionQnA = [
-    {
-        question: "What are your hobbies?",
-        answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda blanditiis delectus laboriosam mollitia suscipit."
-    },
-    {
-        question: "Are you open for work?",
-        answer: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters"
-    },
-    {
-        question: "What's the fastest way to reach you?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
+  {
+    question: "What are your hobbies?",
+    answer: "I enjoy reading playing video games and watching anime and when I'm not playing or watching anime for leisure, I spend my free time learning about random stuff mainly about computers and technology. I also like listening to music."
+  },
+  {
+    question: "What motivates you",
+    answer: "Meeting deadlines, targets and goals, My passion in coding and learning new things while coming up with creative ideas to improve something or create something new."
+  },
+  {
+    question: "Are you open for work?",
+    answer: "Yes. I am always open to work on innovative projects with creative ideas."
+  },
+  {
+    question: "What's the fastest way to reach you?",
+    answer: "Definitely email. Contact me at christiangazzingan38@gmail.com"
+  },
 ]
